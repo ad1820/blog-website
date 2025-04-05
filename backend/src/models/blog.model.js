faae483d740d4bf88a3ade48b27f7856259e1d4a
Aditya <aditya913333@gmail.com>
2025-04-05 17:28:07 +0530

    database models

---
+++ backend/src/models/blog.model.js
@@ -0,0 +1,30 @@
+import mongoose, {Schema} from "mongoose"
+
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: [Schema.Types.ObjectId],
+        ref: "User",
+        default: []
+    },
+    dislikes: {
+        type: [Schema.Types.ObjectId],
+        ref: "User",
+        default: []
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+}, { timestamps: true });
+
+export const Blog = mongoose.model("Blog", blogSchema)
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose, {Schema} from "mongoose"
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    }
+}, { timestamps: true });
+
+
+export const User = mongoose.model("User", userSchema)
