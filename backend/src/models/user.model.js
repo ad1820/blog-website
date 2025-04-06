1aeec9a5732e9811a1caefa2c44ba0880850ab9b
Aditya <aditya913333@gmail.com>
2025-04-06 19:07:56 +0530

    added user model with password hashing and JWT generation

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose, {Schema} from "mongoose"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+
 
 const userSchema = new Schema({
     userName: {
@@ -27,5 +30,23 @@ const userSchema = new Schema({
     }
 }, { timestamps: true });
 
+//pre-save middleware
+userSchema.pre("save", async(next) => {
+    if(!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateJWT = function(){
+    return jwt.sign(
+        {id: this._id},
+        process.env.JWT_SECRET,
+        {expiresIn: "7d"}
+    )
+}
 
 export const User = mongoose.model("User", userSchema)
